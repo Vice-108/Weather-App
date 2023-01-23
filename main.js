98fdd695a8c10d254d46d421b9796aa8661c2b02
Vice <vicemark108@gmail.com>
2023-01-23 20:47:42 +0530

    hello 🗺

---
+++ main.js
@@ -0,0 +1,2 @@
+let link=`http://api.openweathermap.org/geo/1.0/direct?q=${city_name},${state_code},${country_code}&limit=${limit}&appid=${API_key}`;
+let API_key="d7ed544cb380e99eec9211c10aea6efc";
